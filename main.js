a9a902c137e2afc7fbd98e16edce63b657d3872e
tvpaterson <taylorpaterson7@gmail.com>
2022-03-07 18:48:43 +0000

    complete update clock function

--- main.js
+++ main.js
@@ -7,6 +7,38 @@ const timer = {
     longBreakInterval: 4
 };
 
+// FUNCTION TO UPDATE CLOCK WHEN MODE IS CHANGED
+
+function updateClock() {
+    const { remainingTime } = timer;
+    const minutes = "${remainingTime.minutes}".padStarrt(2, "0");
+    const seconds = "${remainingTime.seconds}".padStarrt(2, "0");
+
+    const min = document.getElementById('js-minutes');
+    const seconds = document.getElementById('js-seconds');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+}
+
+// FUNCTION TO SWITCH BETWEEN MODES
+
+function swtichMode(mode) {
+    timer.mode = mode;
+    timer.remainingTime = {
+        total: time[mode] * 60,
+        minutes: timer[mode],
+        seconds: 0,
+    };
+
+    document
+        .querySelectorAll('button[data-mode]')
+        .forEach(e => e.classList.remove('active'));
+    document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
+    document.body.style.backgroundColor = `var(--${mode})`;
+
+  updateClock();
+};
+
 // ADD EVENT LISTENER TO BUTTONS TO BE ABLE TO CHANGE MODES
 
 const modeButtons = document.querySelector("#js-mode-buttons");
@@ -20,4 +52,3 @@ function handleMode(event){
     swtichMode(mode)
 };
 
-// 
